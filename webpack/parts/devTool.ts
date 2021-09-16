d1fa12955bfce1d58f4968d403e85244465ff2c6
ethan-heo <ethan.hb94@gmail.com>
2021-09-16 19:59:09 +0900

    chore: remove webpack and copay and paste resource

--- webpack/parts/devTool.ts
+++
@@ -1,3 +0,0 @@
-export default {
-	devtool: "inline-source-map",
-};
--- webpack/parts/index.ts
+++
@@ -1,5 +0,0 @@
-export * as paths from "./paths";
-export * as rules from "./rules";
-export * as plugins from "./plugins";
-export { default as devServer } from "./devServer";
-export { default as devTool } from "./devTool";
